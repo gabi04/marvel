d9b77e15c03a2003b4cac4544572e99d58f83522
gabi04 <gsuarezc@unal.edu.co>
2023-06-09 11:38:18 -0500

    my fisrt commit

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.sass']
+})
+export class ImgComponent {
+   @Input() img = 'img.component.ts img'
+}
